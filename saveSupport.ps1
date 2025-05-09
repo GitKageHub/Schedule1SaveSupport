e68d4e3887b16d64852487e0ccf0358d5304b9d7
Kyle Green <khgreenprime@gmail.com>
2025-05-09 03:45:15 -0700

    bugfix: remove item needed recurse

--- saveSupport.ps1
+++ saveSupport.ps1
@@ -120,162 +120,203 @@ function Set-LocationSchedule1Saves {
 
 ## Sanity Logic
 
-try {
-    $localDirName = 'S1SS' # goes the snake
-    $localLowPath = "$env:USERPROFILE\AppData\LocalLow"
-    $s1ssPath = Join-Path -Path $localLowPath -ChildPath $localDirName
-    $zipEncode = 'aHR0cHM6Ly9naXRodWIuY29tL0dpdEtBZ2VIdWIvU2NoZWR1bGUxU2F2ZVN1cHBvcnQvYXJjaGl2ZS9yZWZzL2hlYWRzL21hc3Rlci56aXA='
-    $directoryFound = Test-Path -Path $s1ssPath
-    if (-not($directoryFound)) {
-        New-Item -Path $s1ssPath -ItemType Directory -Force -ErrorAction Stop -Verbose
-        try {
-            # Make a temp location
-            $tempDirName = New-Guid #guids avoids collisions, simpler to implement than Get-Random
-            $tempDirPath = Join-Path -Path $env:TEMP -ChildPath $tempDirName
-            New-Item -Path $tempDirPath -ItemType Directory -Force -ErrorAction Stop -Verbose
 
-            # Download zip to temp
-            $zipBytes = [System.Convert]::FromBase64String($zipEncode)
-            $zipUrl = [System.Text.Encoding]::UTF8.GetString($zipBytes)
-            Write-Host "Downloading scripts from GitHub to $tempDirPath" -ForegroundColor Cyan
-            $zipFile = Join-Path -Path $tempDirPath -ChildPath 's1ss_temp.zip'
-            Invoke-WebRequest -Uri $ZipUrl -OutFile $zipFile -ErrorAction Stop -UseBasicParsing -Verbose
+$localDirName = 'S1SS' # goes the snake
+$localLowPath = "$env:USERPROFILE\AppData\LocalLow"
+$s1ssPath = Join-Path -Path $localLowPath -ChildPath $localDirName
+$zipEncode = 'aHR0cHM6Ly9naXRodWIuY29tL0dpdEtBZ2VIdWIvU2NoZWR1bGUxU2F2ZVN1cHBvcnQvYXJjaGl2ZS9yZWZzL2hlYWRzL21hc3Rlci56aXA='
+$directoryFound = Test-Path -Path $s1ssPath
+if (-not($directoryFound)) {
+    New-Item -Path $s1ssPath -ItemType Directory -Force -ErrorAction Stop -Verbose
+    try {
+        # Make a temp location
+        $tempDirName = New-Guid #guids avoids collisions, simpler to implement than Get-Random
+        $tempDirPath = Join-Path -Path $env:TEMP -ChildPath $tempDirName
+        New-Item -Path $tempDirPath -ItemType Directory -Force -ErrorAction Stop -Verbose
 
-            # Extract the contents
-            Write-Host "Extracting scripts to $s1ssPath" -ForegroundColor Cyan
-            Expand-Archive -Path $zipFile -DestinationPath $s1ssPath -Force -ErrorAction Stop
-        }
-        catch {
-            Write-Error "Error downloading or extracting scripts: $($_.Exception.Message)"
-            throw  # Re-throw the error to be caught by calling script
-        }
-        finally {
-            # Clean up
-            Write-Host "Extraction complete, cleaning up temporary files." -ForegroundColor Green
-            Remove-Item -Path $tempDirPath -Force -ErrorAction SilentlyContinue
+        # Download zip to temp
+        $zipBytes = [System.Convert]::FromBase64String($zipEncode)
+        $zipUrl = [System.Text.Encoding]::UTF8.GetString($zipBytes)
+        Write-Host "Downloading scripts from GitHub to $tempDirPath" -ForegroundColor Cyan
+        $zipFile = Join-Path -Path $tempDirPath -ChildPath 's1ss_temp.zip'
+        Invoke-WebRequest -Uri $ZipUrl -OutFile $zipFile -ErrorAction Stop -UseBasicParsing -Verbose
+
+        # Extract the contents
+        Write-Host "Extracting scripts to $s1ssPath" -ForegroundColor Cyan
+        Expand-Archive -Path $zipFile -DestinationPath $s1ssPath -Force -ErrorAction Stop
+    }
+    catch {
+        Write-Error "Error downloading or extracting scripts: $($_.Exception.Message)"
+        throw  # Re-throw the error to be caught by calling script
+    }
+    finally {
+        # Clean up
+        Write-Host "Extraction complete, cleaning up temporary files." -ForegroundColor Green
+        Remove-Item -Path $tempDirPath -Recurse -Force -ErrorAction SilentlyContinue
+    }
+}
+else {
+    # Look for existing vault
+    $vaultPath = Join-Path -Path $s1ssPath -ChildPath 'Vault'
+    $localVaultFound = Test-Path $vaultPath -PathType Container
+    try {
+        $ReadyToSupport = $false
+        while (-not($ReadyToSupport)) {
+            if (-not($localVaultFound)) {
+                ## New Vault
+                New-Item -path $vaultPath -ItemType Directory -Force -ErrorAction Stop -Verbose
+                $ReadyToSupport = $true
+            }
+            else {
+                ## Load Vault
+                class SaveGame {
+                    #Game.json
+                    $GameVersion
+                    $OrganisationName
+                    #Metadata.json
+                    $LastPlayedDate
+                    #Time.json
+                    $ElapsedDays
+                    #Player_0/Inventory.json
+                    $CashBalance
+                    #Money.json
+                    $OnlineBalance
+                    # Path to SaveGame
+                    $pathSaveGame
+                }
+            }
         }
     }
-    else {
-        # Look for existing vault
-        $vaultPath = Join-Path -Path $s1ssPath -ChildPath 'Vault'
-        $localVaultFound = Test-Path $vaultPath -PathType Container
-        try {
-            $ReadyToSupport = $false
-            while (-not($ReadyToSupport)) {
-                if (-not($localVaultFound)) {
-                    ## New Vault
-                    New-Item -path $vaultPath -ItemType Directory -Force -ErrorAction Stop -Verbose
-                    $ReadyToSupport = $true
-                }
-                else {
-                    ## Load Vault
-                    class SaveGame {
-                        #Game.json
-                        $GameVersion
-                        $OrganisationName
-                        #Metadata.json
-                        $LastPlayedDate
-                        #Time.json
-                        $ElapsedDays
-                        #Player_0/Inventory.json
-                        $CashBalance
-                        #Money.json
-                        $OnlineBalance
-                        # Path to SaveGame
-                        $pathSaveGame
-                    }
-                }
-            }
-        }
-        catch {}
-        finally {}
+    catch {}
+    finally {}
 
-        # Eat moar RAM
-        $savepathSchedule1 = Set-LocationSchedule1Saves -Return $true
-        $activeSaves = @()
-        $vaultedSaves = @()
-        $unexpectedSaves = @()
+    # Eat moar RAM
+    $savepathSchedule1 = Set-LocationSchedule1Saves -Return $true
+    $activeSaves = @()
+    $vaultedSaves = @()
+    $unexpectedSaves = @()
 
-        # Active Saves
-        for ($i = 1; $i -le 5; $i++) {
-            $saveFolderName = "SaveGame_$i"
-            $saveGamePath = Join-Path $savepathSchedule1 $saveFolderName
-            if (Test-Path $saveGamePath -PathType Container) {
-                $loadedSave = Get-SaveGame -SaveFolder $saveGamePath
-                if ($loadedSave) {
-                    $activeSaves += $loadedSave
-                }
-            }
-        }
-
-        # Unexpected Saves
-        $expectedSaveFolders = 1..5 | ForEach-Object { "SaveGame_$_" }
-        Get-ChildItem -Path $savepathSchedule1 -Directory | Where-Object { $_.Name -notin $expectedSaveFolders } | ForEach-Object {
-            $unexpectedSavePath = $_.FullName
-            $loadedSave = Get-SaveGame -SaveFolder $unexpectedSavePath
+    # Active Saves
+    for ($i = 1; $i -le 5; $i++) {
+        $saveFolderName = "SaveGame_$i"
+        $saveGamePath = Join-Path $savepathSchedule1 $saveFolderName
+        if (Test-Path $saveGamePath -PathType Container) {
+            $loadedSave = Get-SaveGame -SaveFolder $saveGamePath
             if ($loadedSave) {
-                $unexpectedSaves += $loadedSave
+                $activeSaves += $loadedSave
             }
         }
+    }
 
-        # Vaulted Saves
-        if (Test-Path $vaultPath -PathType Container) {
-            Get-ChildItem -Path $vaultPath -Directory | ForEach-Object {
-                $vaultedSavePath = $_.FullName
-                $loadedSave = Get-SaveGame -SaveFolder $vaultedSavePath
-                if ($loadedSave) {
-                    $vaultedSaves += $loadedSave
-                }
+    # Unexpected Saves
+    $expectedSaveFolders = 1..5 | ForEach-Object { "SaveGame_$_" }
+    Get-ChildItem -Path $savepathSchedule1 -Directory | Where-Object { $_.Name -notin $expectedSaveFolders } | ForEach-Object {
+        $unexpectedSavePath = $_.FullName
+        $loadedSave = Get-SaveGame -SaveFolder $unexpectedSavePath
+        if ($loadedSave) {
+            $unexpectedSaves += $loadedSave
+        }
+    }
+
+    # Vaulted Saves
+    if (Test-Path $vaultPath -PathType Container) {
+        Get-ChildItem -Path $vaultPath -Directory | ForEach-Object {
+            $vaultedSavePath = $_.FullName
+            $loadedSave = Get-SaveGame -SaveFolder $vaultedSavePath
+            if ($loadedSave) {
+                $vaultedSaves += $loadedSave
             }
         }
+    }
 
-        ## Save Support Super System
+    ## Save Support Super System
 
-        $mnemonicLoop = $true
-        while ($true -eq $mnemonicLoop) {
-            # TODO: Menu for user input
-            Write-Host "Schedule 1 Save Support`n"
-            Write-Host 'Make a selection:'
-            Write-Host 'B) Backup a save'
-            Write-Host 'I) Inspect a save'
-            Write-Host 'L) List saves'
-            Write-Host 'M) Modify a save'
-            Write-Host 'R) Restore a save'
-            Write-Host 'Q) Quit'
-            $userInput = Read-Host "Select a number or Q to exit"
-            Clear-Host
-            switch ($userInput.ToUpper()) {
-                'B' {
-                    #TODO: BACKUP a save
-                }
-                'I' {
-                    #TODO: INSPECT a save
-                }
-                'L' {
-                    # List Saves
-                    Write-Host "--- Active Saves ---"
-                    $activeSaves | Format-Table GameVersion, OrganisationName, LastPlayedDate, ElapsedDays, CashBalance, OnlineBalance, pathSaveGame -AutoSize
-
-                    Write-Host "--- Unexpected Saves ---"
-                    $unexpectedSaves | Format-Table GameVersion, OrganisationName, LastPlayedDate, ElapsedDays, CashBalance, OnlineBalance, pathSaveGame -AutoSize
-
-                    Write-Host "--- Vaulted Saves ---"
-                    $vaultedSaves | Format-Table GameVersion, OrganisationName, LastPlayedDate, ElapsedDays, CashBalance, OnlineBalance, pathSaveGame -AutoSize
-                }
-                'M' {
-                    #TODO: MODIFY a save
-                }
-                'R' {
-                    #TODO: RESTORE a save
-                }
-                'Q' { $mnemonicLoop = $false }
-                Default { $mnemonicLoop = $false }
+    $mnemonicLoop = $true
+    while ($true -eq $mnemonicLoop) {
+        # TODO: Menu for user input
+        Write-Host "Schedule 1 Save Support`n"
+        Write-Host 'Make a selection:'
+        Write-Host 'B) Backup a save'
+        Write-Host 'I) Inspect a save'
+        Write-Host 'L) List saves'
+        Write-Host 'M) Modify a save'
+        Write-Host 'R) Restore a save'
+        Write-Host 'Q) Quit'
+        $userInput = Read-Host "Select a number or Q to exit"
+        Clear-Host
+        switch ($userInput.ToUpper()) {
+            'B' {
+                #TODO: BACKUP a save
             }
+            'I' {
+                #TODO: INSPECT a save
+            }
+            'L' {
+                # List Saves
+                Write-Host "--- Active Saves ---"
+                $activeSaves | Format-Table GameVersion, OrganisationName, LastPlayedDate, ElapsedDays, CashBalance, OnlineBalance, pathSaveGame -AutoSize
+
+                Write-Host "--- Unexpected Saves ---"
+                $unexpectedSaves | Format-Table GameVersion, OrganisationName, LastPlayedDate, ElapsedDays, CashBalance, OnlineBalance, pathSaveGame -AutoSize
+
+                Write-Host "--- Vaulted Saves ---"
+                $vaultedSaves | Format-Table GameVersion, OrganisationName, LastPlayedDate, ElapsedDays, CashBalance, OnlineBalance, pathSaveGame -AutoSize
+            }
+            'M' {
+                #TODO: MODIFY a save
+            }
+            'R' {
+                #TODO: RESTORE a save
+            }
+            'Q' { $mnemonicLoop = $false }
+            Default { $mnemonicLoop = $false }
         }
     }
 }
-catch {
-    Write-Error "An error occurred: $($_.Exception.Message)"
-    Write-Host "Script execution halted." -ForegroundColor Red
-    exit 2 # "But Fry, there's no such thing as 2!" -Bender
+try {
+    $timeComplete = Get-Date
+}
+finally {
+    
+    # Calculate the time difference
+    $timeTaken = $timeComplete - $timeStarted
+
+    # Output the result in total seconds
+    Write-Host "Script executed in $($timeTaken.TotalSeconds) seconds."
+
+    # Output the result in a more human-readable format
+    Write-Host "Script execution time: $($timeTaken.Days) days, $($timeTaken.Hours) hours, $($timeTaken.Minutes) minutes, $($timeTaken.Seconds) seconds, $($timeTaken.Milliseconds) milliseconds."
+
+    # A slightly more natural English output (Handles singular/plural)
+    $days = $timeTaken.Days
+    $hours = $timeTaken.Hours
+    $minutes = $timeTaken.Minutes
+    $seconds = $timeTaken.Seconds
+    $milliseconds = $timeTaken.Milliseconds
+
+    $dayString = "day"
+    if ($days -ne 1) { $dayString = "days" }
+    $hourString = "hour"
+    if ($hours -ne 1) { $hourString = "hours" }
+    $minuteString = "minute"
+    if ($minutes -ne 1) { $minuteString = "minutes" }
+    $secondString = "second"
+    if ($seconds -ne 1) { $secondString = "seconds" }
+    $millisecondString = "millisecond"
+    if ($milliseconds -ne 1) { $millisecondString = "milliseconds" }
+
+    Write-Host "Script took: " -NoNewline
+    if ($days -gt 0) {
+        Write-Host "$days $dayString, " -NoNewline
+    }
+    if ($hours -gt 0) {
+        Write-Host "$hours $hourString, " -NoNewline
+    }
+    if ($minutes -gt 0) {
+        Write-Host "$minutes $minuteString, " -NoNewline
+    }
+    if ($seconds -gt 0) {
+        Write-Host "$seconds $secondString, " -NoNewline
+    }
+    Write-Host "$milliseconds $millisecondString."
 }
